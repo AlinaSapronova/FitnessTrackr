b015ea8ca674b1f2576ff0275478b38252619945
Ruby Gonzalez <RubyJ.Gonzalez@outlook.com>
2022-07-25 12:56:26 -0500

    getUserbyUsername

--- db/users.js
+++ db/users.js
@@ -1,52 +1,92 @@
 const client = require("./client");
 
-
 const bcrypt = require("bcrypt");
 
 // database functions
 
 // user functions
 async function createUser({ username, password }) {
-   const SALT_COUNT = 10;
+  const SALT_COUNT = 10;
   const hashedPassword = await bcrypt.hash(password, SALT_COUNT);
-  try{
-  const {
-    rows: [user],
-  } = await client.query(
-    `
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
       INSERT INTO users(username, password) 
       VALUES($1, $2) 
       ON CONFLICT (username) DO NOTHING 
       RETURNING *;
     `,
-    [username, hashedPassword ]
-  );
-  return user;
-  }catch(error) {
-    console.log("CreateUsers errors")
-    throw error
+      [username, hashedPassword]
+    );
+    return user;
+  } catch (error) {
+    console.log("CreateUsers errors");
+    throw error;
   }
 }
 
+//get user - this hsould be able to verify the password against the hashed password.
 async function getUser({ username, password }) {
+  const user = await getUserByUserName(username);
+  const hashedPassword = user.password;
+  const isValid = await bcrypt.compare(password, hashedPassword);
   try {
+    const {rows}= await client.query(
+      `
+      INSERT INTO users(username, password) 
+      VALUES($1, $2) 
+      ON CONFLICT (username) DO NOTHING 
+      RETURNING *;
+    `,
+      [username, isValid]
+    );
+    return rows;
 
- 
-const user = await getUserByUserName(username);
-const hashedPassword = user.password;
-const isValid = await bcrypt.compare(password, hashedPassword)
- }catch(error) {
-  console.log("error getUser")
-  throw error
- }
+  } catch (error) {
+    console.log("error getUser");
+    throw error;
+  }
 }
 
 async function getUserById(userId) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(`
+    SELECT id, username,
+    FROM users,
+    WHERE id = ${userId} 
+    `);
 
+    // user.posts = await getPostsByUser(userId);
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
 }
 
-async function getUserByUsername(userName) {
 
+
+async function getUserByUsername(username) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+      SELECT *
+      FROM users
+      WHERE username=$1;
+    `,
+      [username]
+    )
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
 }
 
 module.exports = {
@@ -54,4 +94,4 @@ module.exports = {
   getUser,
   getUserById,
   getUserByUsername,
-}
+};
