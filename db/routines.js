d2a1efe1671a7210f23405734b6d17e32dd355ae
Ruby Gonzalez <RubyJ.Gonzalez@outlook.com>
2022-07-27 08:57:40 -0500

    Finished updateRoutine

--- db/routines.js
+++ db/routines.js
@@ -84,18 +84,58 @@ async function getAllRoutinesByUser({ username }) {
 
 }
 
-
 async function getPublicRoutinesByUser({ username }) {
-
+  try{
+    const {rows:routines} = await client.query(`
+        SELECT routines.*, users.username AS "creatorName"
+        FROM routines
+        JOIN users ON routines."creatorId" = users.id
+        WHERE username = $1 AND routines."isPublic" = true;
+    `, [username]);
+    return attachActivitiesToRoutines(routines);
+    }catch(error) {
+      console.error("getAllRoutines errors")
+      throw error
+    } 
 }
 
 async function getPublicRoutinesByActivity({ id }) {
-
+  try{
+    const {rows:routines} = await client.query(`
+        SELECT routines.*, users.username AS "creatorName"
+        FROM routines
+        JOIN users ON routines."creatorId" = users.id
+        JOIN routine_activities ON routine_activities."routineId" = routines.id
+        WHERE routine_activities."activityId" = ${id} AND routines."isPublic" = true;
+    `,);
+    return attachActivitiesToRoutines(routines);
+    }catch(error) {
+      console.error("getAllRoutines errors")
+      throw error
+    } 
   
 }
 
 async function updateRoutine({ id, ...fields }) {
-
+  const setString = Object.keys(fields)
+  .map((key, index) => `"${key}"=$${index + 1}`)
+  .join(", ");
+ try{ 
+const{rows:[routine]} =
+    await client.query(
+      `
+      UPDATE routines
+      SET ${setString}
+      WHERE id=${id}
+      RETURNING *;
+    `,
+      Object.values(fields)
+    );
+    return routine
+   } catch (error) {
+      console.log("There is an error in updateRoutine");
+       throw error;
+ }
 }
 
 async function destroyRoutine(id) {}
